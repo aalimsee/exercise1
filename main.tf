b83bde60139950530c7b34e084bdfed01a4c1d69
aaron lim <aaron.limse@hotmail.com>
2025-01-15 21:51:00 +0800

    force_destroy = True for terraform destroy to work

--- main.tf
+++ main.tf
@@ -5,11 +5,12 @@ terraform{
     key    = "Users/aaronlim/ce9-exercises/exercise1/terraform.tfstate" # Replace the value of key to <your suggested name>.tfstate for example terraform-ex-ec2-<NAME>.tfstate
     region = "us-east-1"
   }
-
 }
+
 # Create AWS S3
 resource "aws_s3_bucket" "bucket" {
   bucket = "aalimsee-ce9-test-bucket" # Change to a globally unique name
+  force_destroy = true
 
   tags = {
     Name        = "aalimsee Bucket" # Change to your own name
